3393caf2eb632e18eaa032c0864dacd99985c913
Yesha_Joshi <ysjoshi0311@gmail.com>
2024-03-27 20:07:34 +0530

    Co-authored-by: Rujal-Jiyani <Rujal-Jiyani@users.noreply.github.com>input function

--- DemoDS.cpp
+++ DemoDS.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 struct table
 {
     int branch;
@@ -70,27 +69,95 @@ class Year
     }
 
     void setYear(int c, int r, int i, string n);
+
+    void set_yr(int y){
+        yr=y;
+    }
 };
 
 class DataBase
 {
     private:
     Year *year;
+    int yr1;
 
     public:
-    DataBase(){}
+    DataBase(){
+        year = NULL;
+    }
     DataBase(int y)
     {
         year = new Year[y];
+        yr1 = y;
     }
     void allocateYearMemory(int y)
     {
         year = new Year[y];
+        yr1 = y;
     }
 
     void setData(int y, int c, int r, int i, string n){
         year[y].setYear(c,r,i,n);
     }
+
+    Year * hashRtYear(int y)
+    {
+        return &year[y%yr1];
+    }
 };
 
 
+    
+
+void set_data(){
+
+    ifstream my_yr_file;
+    my_yr_file.open("yearFile.txt");
+    int No_yr = 0;
+
+    while(my_yr_file.peek() != EOF)//Gets the number of year
+    {   
+        string year;
+        getline(my_yr_file, year, '\n');
+        No_yr++;
+    }
+
+    DataBase All_std_data(No_yr); 
+
+    my_yr_file.seekg(0 , my_yr_file.beg);
+    
+    for(int i=0 ; i<No_yr ; i++)
+    {
+        ifstream my_comp_file;
+
+        string yearF;
+        getline(my_yr_file, yearF, '\n');
+
+        int int_year=0;//string year to int year
+        while(yearF[i]<4){
+            int temp;
+            temp=yearF[i]* pow(10,3-i);
+            int_year= int_year+temp;
+        }
+
+        Year *yptr = All_std_data.hashRtYear(int_year);
+
+        yptr->set_yr(int_year);
+
+        my_comp_file.open(yearF);
+
+        int No_c;
+
+        while(my_comp_file.peek() != EOF)//Gets the number of company*5
+        {   
+            string c;
+            getline(my_comp_file, c, '\n');
+            No_c++;
+        }
+
+        No_c = No_c/5;//Gets the number of company
+
+        yptr->allocateCompMemory(No_c);
+        
+    }
+}
